25151377cc44c55f9cae86ea3d1836d70da67ad5
Badwargame37 <146677593+Badwargame37@users.noreply.github.com>
2023-10-16 09:57:14 +0200

    Create rbash.cpp

---
+++ DockerCompose/pivot/rbash.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+void executeCommand(const std::string& command) {
+    system(command.c_str());
+}
+
+int main() {
+    std::string input;
+    while (true) {
+        std::cout << "rbash$ ";
+        std::getline(std::cin, input);
+
+        if (input == "commande get-ip") {
+            executeCommand("hostname -I");
+        } 
+        else if (input.substr(0, 14) == "commande chisel") {
+            std::string chiselArgs = input.substr(15);
+            executeCommand("/opt/chisel " + chiselArgs);
+        } 
+        else if (input == "help") {
+            std::cout << "Available commands:" << std::endl;
+            std::cout << "commande get-ip -> Get the IP" << std::endl;
+            std::cout << "commande chisel -> Execute /opt/chisel with arguments" << std::endl;
+            std::cout << "help -> Show help" << std::endl;
+            std::cout << "exit -> Exit rbash" << std::endl;
+        } 
+        else if (input == "exit") {
+            break;
+        } 
+        else {
+            std::cout << "Unknown command. Type 'help' for a list of available commands." << std::endl;
+        }
+    }
+    return 0;
+}
