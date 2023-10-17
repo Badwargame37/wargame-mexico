c820b6006fc2fe19ac7c13701bcde9acbcd11a8d
Badwargame37 <146677593+Badwargame37@users.noreply.github.com>
2023-10-17 02:11:44 +0200

    Delete DockerCompose/Reverse/reverse directory

--- DockerCompose/Reverse/reverse/passwordcreate.py
+++
@@ -1,7 +0,0 @@
-from werkzeug.security import generate_password_hash
-
-passwords = ["Flag123", "internet", "500379d15e1f3c1ca605168b62f367cbdab8abde2b64f236972903f9ff3f63a5", "tequiero"]
-
-hashed_passwords = [generate_password_hash(password) for password in passwords]
-
-print("Hashed Passwords: ", hashed_passwords)
